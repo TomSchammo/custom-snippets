b0a8ba04123f6e55352382c55da34c8b78419277
Tom Schammo <tom@tomschammo.com>
2026-04-29 20:53:17 +0200

    feat: New C++ snippets
    `.fmt` and `.print` can now be used to transform these:
    
    `"hello {name}".fmt` -> `std::format("hello {}", name)`,
    `"hello {name}".print` -> `std::println("hello {}", name)`
    
    NOTE: I used Claude code to generate this snippet

--- luasnippets/cpp.lua
+++ luasnippets/cpp.lua
@@ -1,3 +1,7 @@
+local ls = require("luasnip")
+local f = ls.function_node
+local postfix = require("luasnip.extras.postfix").postfix
+
 local function get_shared_snippets()
     local current_file = debug.getinfo(1, "S").source:sub(2) -- Remove @ prefix
     local current_dir = current_file:match("(.*/)")
@@ -10,5 +14,40 @@ local function get_shared_snippets()
     end
 end
 
-return get_shared_snippets()
+local function parse_fmt_string(match)
+    local vars = {}
+    local fmt_str = match:gsub("{([^}]+)}", function(v)
+        table.insert(vars, v)
+        return "{}"
+    end)
+    return fmt_str, vars
+end
 
+local fmt_snippet = postfix(
+    { trig = ".fmt", match_pattern = [["[^"]*"]] },
+    {
+        f(function(_, parent)
+            local match = parent.snippet.env.POSTFIX_MATCH
+            local fmt_str, vars = parse_fmt_string(match)
+            return string.format("std::format(%s, %s);", fmt_str, table.concat(vars, ", "))
+        end, {})
+    }
+)
+
+local print_snippet = postfix(
+    { trig = ".print", match_pattern = [["[^"]*"]] },
+    {
+        f(function(_, parent)
+            local match = parent.snippet.env.POSTFIX_MATCH
+            local fmt_str, vars = parse_fmt_string(match)
+            if #vars == 0 then
+                return string.format("std::println(%s);", match)
+            end
+            return string.format("std::println(%s, %s);", fmt_str, table.concat(vars, ", "))
+        end, {})
+    }
+)
+
+local shared = get_shared_snippets()
+vim.list_extend(shared, { fmt_snippet, print_snippet })
+return shared
